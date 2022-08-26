1c7457ec94df632d9bead67fcf57b556d59a7835
Chris Bradford <chrismbradford@outlook.com>
2022-08-26 19:42:20 +0100

    Finalise Redis client API migration settings.

--- src/loaders/redis-limiter.js
+++ src/loaders/redis-limiter.js
@@ -7,25 +7,10 @@ var logger = require('./logger'); // Moved to own module
 ///////////////////////////////////////////////////////////////////////////
 var redisHost = process.env.REDIS_SERVER || 'redis';
 var client = require('redis').createClient({
-    url: `redis://${redisHost}:6397/0`,
-    // db: 0,
-	retry_strategy: function (options) {
-        if (options.error && options.error.code === 'ECONNREFUSED') {
-			return new Error('The server refused the connection');
-        }
-        if (options.total_retry_time > 1000 * 60 * 60) {
-			//logger.log('error', "[REDIS] Retry time exhausted");
-			return new Error('Retry time exhausted');
-        }
-        if (options.attempt > 100) {
-			// End reconnecting with built in error
-			logger.log('error', "[Redis] Redis server connection retry limit exhausted");
-            return undefined;
-        }
-		// reconnect after
-		//logger.log('error', "[REDIS] Attempting reconnection after set interval");
-        return Math.min(options.attempt * 1000, 10000);
-   	}
+    url: `redis://${redisHost}:6379/0`,
+    reconnectStrategy() {
+        return 3000;
+    }
 });
 client.on('connect', function() {
     logger.log('info', "[Redis] Connecting to Redis server...");
